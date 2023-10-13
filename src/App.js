b51e54cdf5b61d28b0c252f59f3ffec6431f3f6a
Ram0399 <118108414+Ram0399@users.noreply.github.com>
2023-10-13 20:06:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice"; // Corrected import path
 
 function App() {
+  const user = useSelector(selectUser); // Get the user state from Redux
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged In
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // Not logged in
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          {!user ? (
+            <Route path="/" element={<LoginScreen />} />
+          ) : (
+            <>
+              <Route index element={<HomeScreen />} />
+            </>
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAQuyrnmLk6Oyc9IYmSKM_C8gluwcbcnwg",
   authDomain: "netlfix-clone-b3a54.firebaseapp.com",
@@ -8,3 +11,9 @@ const firebaseConfig = {
   appId: "1:207121307853:web:d580bec14fb33eead27184",
   measurementId: "G-0SBDCYK0VK",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { auth, db };
